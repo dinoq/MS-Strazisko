2d6b11754a3ed3bb50f51fbb66ab339188f8bfee
Petr Marek <marek.petr10@seznam.cz>
2021-08-27 20:29:11 +0200

    Working on file uploading

--- pages/api/file.ts
+++ pages/api/file.ts
@@ -10,18 +10,29 @@ export const config = {
 const post = async (req, res) => {
   const form = new formidable.IncomingForm();
   form.parse(req, async function (err, fields, files) {
-    await saveFile(files.file);
+    console.log('files: ', files);
+    console.log('fields: ', fields.path);
+    let keys = Object.keys(files);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      await saveFile(files[key], fields.path);
+      
+    }
     return res.status(201).send("");
   });
 };
 
-const saveFile = async (file) => {
-  const data = fs.readFileSync(file.path);
-  fs.writeFileSync(`./public/${file.name}`, data);
+const saveFile = async (file, path) => {
+  console.log('file.path: ', file.path);
+  const data = await fs.readFileSync(file.path);
+  const fullPath = "./public/" + (path.startsWith("/")? "" : "/") + path + (path.endsWith("/")? "" : "/") + file.name;
+  console.log('fullPath: ', fullPath);
+  await fs.writeFileSync(fullPath, data);
   await fs.unlinkSync(file.path);
   return;
 };
 
+//eslint-disable-next-line
 export default (req, res) => {
   req.method === "POST"
     ? post(req, res)
--- pages/uploader.tsx
+++ pages/uploader.tsx
@@ -1,38 +1,72 @@
 import { useState } from "react";
+import Image from "next/image"
+import { useRef } from "react";
 
 /**
  * # MY ACCOUNT GOOGLE PLAY:
  * @see {@link https://play.google.com/store/apps/developer?id=dzino Google Play}
  */
 
-const PrivatePage = (props)=> {
+const PrivatePage = (props) => {
   const [image, setImage] = useState(null);
-  const [createObjectURL, setCreateObjectURL] = useState(null);
+  const [createObjectURL, setCreateObjectURL] = useState("/");
+  const preview = useRef<HTMLDivElement>();
+
+
+  const [images, setImages] = useState([]);
 
   const uploadToClient = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      const i = event.target.files[0];
-
-      setImage(i);
-      setCreateObjectURL(URL.createObjectURL(i));
+    console.log('event.target.files: ', event.target.files);
+    if (event.target.files) {      
+      if( Array.from(event.target.files).length === 1){
+        const i = event.target.files[0];
+        console.log('i: ', i);
+        console.log('URL.createObjectURL(i): ', URL.createObjectURL(i));
+  
+        setImage(i);
+        //setCreateObjectURL("/"+i.name);
+        let img = (preview?.current?.children[0] as HTMLImageElement)
+        img.src = URL.createObjectURL(i);
+      }else 
+      if( Array.from(event.target.files).length > 1){
+        console.log("multiple: ", event.target.files.lenght);
+        setImages([]);
+  
+        let imgs = new Array();
+        for (let i = 0; i < event.target.files.length; i++) {
+          const file = event.target.files[i];
+          imgs.push(file);
+        }
+        setImages(imgs);
+      }
     }
+    
   };
 
   const uploadToServer = async (event) => {
     const body = new FormData();
     body.append("file", image);
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i];
+      body.append("file-" + i, img);
+    }
+    body.append("path", "/img/albums/1");
     const response = await fetch("/api/file", {
       method: "POST",
       body
     });
   };
 
+  const size = "400px";
   return (
     <div>
       <div>
-        <img src={createObjectURL} />
+        <div ref={preview} className="" style={{ backgroundColor: "red", width: size, height: "fit-content" }}>
+          {// eslint-disable-next-line @next/next/no-img-element
+            <img alt="" src={createObjectURL} style={{ width: "100%", maxHeight: "100%", objectFit: "contain" }}></img>}
+        </div>
         <h4>Select Image</h4>
-        <input type="file" name="myImage" onChange={uploadToClient} />
+        <input type="file" name="myImage" onChange={uploadToClient} multiple />
         <button
           className="btn btn-primary"
           type="submit"
